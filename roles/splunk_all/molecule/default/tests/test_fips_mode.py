f8b99786df767a389a09e8dd44042ff43818a5dd
Lee Goodrich <goodrich.lee@gmail.com>
2020-07-04 11:22:35 -0700

    flake caught another line too long

--- roles/splunk_all/molecule/default/tests/test_fips_mode.py
+++ roles/splunk_all/molecule/default/tests/test_fips_mode.py
@@ -7,4 +7,5 @@ testinfra_hosts = takeltest.hosts()
 def test_admin_login(host, testvars):
     v = testvars['splunk_all_fips_mode']
     assert v == 'false'
-    assert not host.file("/opt/splunk/etc/splunk-launch.conf").contains('SPLUNK_FIPS = true')
+    assert not host.file(
+        "/opt/splunk/etc/splunk-launch.conf").contains('SPLUNK_FIPS = true')
